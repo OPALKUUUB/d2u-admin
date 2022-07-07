18b4e3dc9e0b483de39fcc158968391048820565
PhyXerSs <kuntaice28@gmail.com>
2022-07-08 00:44:58 +0700

    ewelcia and promotion page

--- client/src/pages/mart/Promotion/App.js
+++ client/src/pages/mart/Promotion/App.js
@@ -1,8 +1,8 @@
 import React, { useContext } from "react";
 import { PromotionContext } from "./PromotionProvider";
-import Layout from "./component/Layout";
-import Filter from "./component/Filter/Filter";
-import { Table } from "./component/Table/Table";
+import Layout from "../Ewelcia/component/Layout";
+import Filter from "../Ewelcia/component/Filter/Filter";
+import { Table } from "../Ewelcia/component/Table/Table";
 
 export const App = () => {
   const { promotions, setShow } = useContext(PromotionContext);
